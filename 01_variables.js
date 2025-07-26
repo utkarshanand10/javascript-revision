fb3a633cad17e5420af1bbb0ffdec9634ba4da07
Utkarsh Anand <utkarshanand133@gmail.com>
2025-07-26 12:15:00 +0530

    Day-1

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1334;
+let accoutEmail = "utkarshanand133@gmail.com";
+var accountPassword = "12344";
+accountCity = "Jaipur";
+
+// accountId = 2
+
+accountEmail = "@gmail.com";
+accountPassword = "15435";
+accountCity = "Delhi";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
